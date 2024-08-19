9968601fb36781a4a4735e0ee782e14102b02e8e
JihanHayaMufialdo <92241700+JihanHayaMufialdo@users.noreply.github.com>
2024-08-19 13:55:50 +0700

    Menambahkan tampilan halaman tentang kami

---
+++ src/views/tentang-kami.php
@@ -0,0 +1,57 @@
+<?php include '../layout/header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang Kami</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../output.css" />
+</head>
+<body class="p-16">
+    <div class="mt-16 text-center">
+        <div class="my-16">
+            <h1 class="title">
+                Tentang Kami
+            </h1>
+            <div class="flex flex-col items-center justify-center">
+                <div class="rounded overflow-hidden shadow-md">
+                    <img src="../img/foto-hotel.jpg" alt="Foto Hotel" class="object-cover" width="500" height="367">
+                </div>
+
+                <div class="ml-12 flex items-center text-primary text-md">           
+                    <address>
+                        <p class="font-semibold">
+                            Alamat:
+                        </p> 
+                        <p class="mb-4">
+                            Jalan Prof. Dr Jl. Prof. Dr. Ir. Sumantri Brojonegoro No.1
+                            <br>
+                            Kota Bandar Lampung, Lampung 35141
+                        </p>
+                        <p class="font-semibold">
+                            Telepon: 
+                        </p>
+                        <p class="mb-4">
+                            <a href="tel:+5555555555">
+                                555-555-5555
+                            </a>
+                        </p>
+                        <p class="font-semibold">
+                            Email:
+                        </p>
+                        <p> 
+                            <a href="mailto:hotel@jwd.co">
+                                hotel@jwd.co
+                            </a>
+                        </p>
+                    </address>
+                </div> 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php include '../layout/footer.php'; ?>
